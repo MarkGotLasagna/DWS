dbfad013fc473fca4b2b3e9d145307b8e7e7b3a6
RossoMalpelooo <ongarileonardo@gmail.com>
2024-03-22 18:39:23 +0100

    detached normalize code

--- MATLAB/main.m
+++ MATLAB/main.m
@@ -45,3 +45,11 @@ figure('Name','TEST','NumberTitle','off')
 
 % p300_plotPeaks(Pic1,7101:7600, 20);
 p300_plotPeaks(Pic2,1:10000, 20);
+
+%% NORMALIZE
+figure('Name','normalize - Pic1','NumberTitle','off')
+% event in [7100:7600]
+normalize(Pic1,1:10000, 20);
+
+figure('Name','normalize - Pic2','NumberTitle','off')
+normalize(Pic2,1:10000, 20);
---
+++ MATLAB/normalize.m
@@ -0,0 +1,82 @@
+%% To plot functions of maximum factors of columns (200) over rows (10000)
+%
+% This function is to be included in files such as 'main.m'
+% to highlight transient peaks positions present in pictures taken by the line camera.
+%
+
+% 'region'      The region of the image to be cropped and plotted
+function [] = normalize(filename, region, sigma)
+    V = tiffreadVolume(filename); % It is advised to put .tif in the same folder as the executable
+    
+    V_crop = V((region),:);
+
+    % Typecast V_crop to double
+    V_crop = double(V_crop);
+
+    % Too much noise
+    % V_crop_max = max(V_crop,[],2)'; 
+
+    V_crop_prctile = prctile(V_crop',99)';
+    V_crop_prctile1 = prctile(V_crop',1)';
+
+    V_crop_prctile = imgaussfilt(V_crop_prctile,sigma);
+    V_crop_prctile1 = imgaussfilt(V_crop_prctile1,sigma);
+
+
+    %%LOCAL NORMALIZING PROCESS: 
+    % x = (x - x_min) / (x_max - x_min);
+
+    % Create the matrix with all columns equal to the given array
+    [num_rows, num_cols] = size(V_crop);
+    V_crop_prctile1 = V_crop_prctile1 * ones(num_cols, 1)';
+    
+    % Subtract 1st percentile (x_min)
+    V_norm = V_crop - V_crop_prctile1;
+    
+    % Divide by the difference between
+    % 99th percentile and 1st percentile (x_max - x_min)
+    V_norm = V_norm ./ (V_crop_prctile - V_crop_prctile1);
+
+
+    %%SMOOTHING PROCESS:
+    % Gaussian smoothing on every columns +
+    % median smoothing on average change (to mantain motion shapes)
+
+    % Generate 1D Gaussian kernel
+    sigma = 1;
+    kernel_size = ceil(3 * sigma) * 2 + 1; % Choose kernel size based on sigma
+    gaussian_kernel = fspecial('gaussian', [kernel_size, 1], sigma);
+    
+    % Perform Gaussian filtering on every vector along the first coordinate
+    V_norm_smooth = conv2(V_norm, gaussian_kernel, 'same'); % 'valid'?
+
+    blocks = 10; % Try different values to see "avg_change" noise variation
+    s = size(V_norm_smooth);
+    avg_change(s(1) - blocks) = 0; % avg_change = [];
+
+    % Find average change for each subsequent row
+    for i = 1:s(1) - blocks
+        change = V_norm_smooth(i + blocks, :) - V_norm_smooth(i, :);
+        avg_change(i) = norm(change);
+    end
+
+    % Window size for the median filter
+    window_size = 21; % Adjust as needed
+    
+    % Apply the median filter using medfilt2
+    avg_filtered_matrix = medfilt2(avg_change, [1, window_size]);
+    
+    % Convert the filtered matrix back to a vector
+    avg_filtered_vector = avg_filtered_matrix(:)';
+
+    subplot(1,2,2)
+    plot(avg_filtered_vector,'b-');
+    xlabel('time');
+    ylabel('change of motion');
+
+    subplot(1,2,1)
+    imagesc(V_norm_smooth)
+    xlabel('pixels');
+    ylabel('time');
+    colorbar;
+end
