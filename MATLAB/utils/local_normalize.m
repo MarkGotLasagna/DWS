536242ffb774811a4607ea242463972e7f63a222
RossoMalpelooo <ongarileonardo@gmail.com>
2024-05-13 17:28:37 +0200

    added documentation
    added disclaimers for column_smooth and local_normalize utilites

--- MATLAB/utils/local_normalize.m
+++ MATLAB/utils/local_normalize.m
@@ -1,17 +1,20 @@
-%% LOCAL NORMALIZING: 
-% x = (x - x_min) / (x_max - x_min);
+function V_norm = local_normalize(V, v01, v99)
+% LOCAL_NORMALIZE  Normalizes data in V using v01 and v99 as min value and
+% max value: x = (x - x_min) / (x_max - x_min);
+% V_norm = local_normalize(V, v01, v99);
+%
+%   See also COLUMN_SMOOTH.
 
-function V_norm = local_normalize(V, v1, v99)
     % Create the MATRIX with all columns equal to the given array
     [~, num_cols] = size(V);
-    V1 = v1 * ones(num_cols, 1)';
+    V01 = v01 * ones(num_cols, 1)';
     
     % Subtract 1st percentile (x_min)
-    V = V - V1;
+    V = V - V01;
     
     % Divide by the difference between
     % 99th percentile and 1st percentile (x_max - x_min)
-    V = V ./ (v99 - V1);  % warning: array - matrix (= matrix)
+    V = V ./ (v99 - V01);  % warning: array - matrix (= matrix)
 
     % Clip values in range [0, 1]
     V_norm = clip(V, 0, 1);
