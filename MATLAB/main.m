f5f0f690a0828b1c858027811b1062c081115ef4
RossoMalpelooo <ongarileonardo@gmail.com>
2024-07-31 13:24:46 +0200

    adjusted fit
    reached stable fit function

--- MATLAB/main.m
+++ MATLAB/main.m
@@ -11,12 +11,13 @@ pics = ["PICS/Picture1.tif" "PICS/Picture2.tif" "PICS/Picture3.tif" ...
     "PICS/Picture4.tif" "PICS/Picture5.tif" "PICS/Picture6.tif"];
 
 % Load sample from PASTA line camera data
-path = "C:/Users/ongar/Pictures/PASTA_Linecamera_Data/";
+% 
 % img = "0038_S101A2-02_R000_2_000103_20220702_080957_000000549.tif";
 % img = "0038_S101A2-02_R000_2_000104_20220702_080957_000000549.tif";
 % img = "0038_S101A2-02_R000_2_000108_20220702_080958_000000549.tif";
-img = "0038_S101A2-02_R000_2_000111_20220702_080959_000000549.tif";
-%%% img = "0038_S101A2-02_R000_2_000114_20220702_081001_000000549.tif";
+ img = "0038_S101A2-02_R000_2_000111_20220702_080959_000000549.tif";
+path = "C:/Users/ongar/Pictures/PASTA_Linecamera_Data/";
+% img = "0038_S101A2-02_R000_2_000114_20220702_081001_000000549.tif";
 
 img_name = path + img;
 
@@ -25,22 +26,29 @@ Im1 = 'PICS/NA.png';
 row_sigma = 200; % high values to reduce noise
 col_sigma = 1;
 
-% coeff_plot_peaks(pics(4), 7500:1:9500, row_sigma, col_sigma, "Kendall");
-% coeff_plot_peaks(pics(5), 5500:1:8500, row_sigma, col_sigma, "Kendall");
+% find_correlations(pics(4), 7500:1:9500, row_sigma, col_sigma, "Kendall");
+% find_correlations(pics(5), 5500:1:8500, row_sigma, col_sigma, "Kendall");
 
 [correlations, image] = find_correlations(img_name, 'all', row_sigma, col_sigma, "Kendall");
-plot(correlations);
+plot(correlations); % data.jpeg
+grid on;
 
-l = 0.4;
+l = 0.6;
 zones = find_zones(correlations, l);
 
-[x, y] = find_peaks(correlations, zones);
+[x_real, y] = find_peaks(correlations, zones);
 
 for i=1:length(y)
-    figure;
-    plot(x{i}, 1-y{i});
-    f = fit(x{i},1-y{i},'exp2');
+    x = 0:1:size(y{i}, 1)-1;
+    fun = @(a,b,c,x) b.*(1-c.*exp(-a.*x));
+    fitfun = fittype( fun );
+    f = fit(x', y{i}, fitfun, 'StartPoint', [5 0.5 0.5], 'Lower', [0 0 0], 'Upper', [100 1 1]);
     hold on;
-    plot(f);
-    title(img + " - " + i, "Interpreter", "none");
+    coeffvals = coeffvalues(f);
+    y_real = fun(coeffvals(1), coeffvals(2), coeffvals(3), x)';
+    plot(cell2mat(x_real(i)), y_real, LineWidth=3);
+    legend_info{i} = "a = " + coeffvals(1);
 end
+legend(["data" legend_info]);
+legend('Location','southeast')
+title(img, "Interpreter", "none");
--- MATLAB/utils/find_peaks.m
+++ MATLAB/utils/find_peaks.m
@@ -1,4 +1,4 @@
-function [x, p] = find_peaks(correlations, zones)
+function [x, y] = find_peaks(correlations, zones)
     peaks = cell(1, length(zones));
     points = cell(1, length(zones));
     for i=1:length(zones)
@@ -7,9 +7,23 @@ function [x, p] = find_peaks(correlations, zones)
         min_val = min(corrs);
         event_start = find(correlations == min_val);
         event_end = event_start + 1500;
+        if(event_start > size(correlations, 1) - 1500)
+            event_end = size(correlations, 1);
+        end
+        points{i} = (event_start:event_end)';
+        peaks{i} = correlations(event_start:event_end);
+    end
+
+    for i=1:length(points)-1
+        event_start = points{i}(1);
+        event_end = points{i}(end);
+        next = zones{i+1}(1);
+        if(event_end > next)
+            event_end = next-75;
+        end
         points{i} = (event_start:event_end)';
         peaks{i} = correlations(event_start:event_end);
     end
     x = points;
-    p = peaks;
+    y = peaks;
 end
